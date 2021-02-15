e10a4ddc45884e8d62a7f182e5d19cdcb9faf4b0
hoanganh-do <hoangganhh.do@gmail.com>
2021-02-15 23:52:23 +0700

    commit all fl

---
+++ controllers/employeeController.js
@@ -0,0 +1,116 @@
+const express= require('express');
+var router= express.Router();
+
+const mongoose= require('mongoose');
+const Employee= mongoose.model('Employee');
+
+
+
+
+router.get('/', (req, res) => {
+    res.render("employee/addOredit", {
+        viewTitle: "Insert Employee"
+    });
+});
+
+router.post('/', (req, res) => {
+//    console.log(req.body);
+      if(req.body._id== '') {
+      insertRecord (req, res);
+      }
+      else {
+      updateRecord (req, res);
+      }
+});
+
+function insertRecord (req, res) {
+   var employee= new Employee();
+   employee.fullName= req.body.fullName;
+   employee.email= req.body.email;
+   employee.mobile= req.body.mobile;
+   employee.city= req.body.city;
+   employee.save((err, doc) => {
+        if(!err) {
+            res.redirect('employee/list');
+       }
+        else {
+        //    if(err.name== 'ValidationError') {
+        //        handleValidationError(err, req.body);
+        //        res.render("employee/addOredit", {
+         //           viewTitle: "Insert Employee",
+        //            employee: req.body
+
+        //    });
+        //}
+        //    else {
+            console.log('Error during record insertion :' + err)}
+        //}
+        
+    });
+}
+
+function updateRecord (req, res) {
+    Employee.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}, (err, doc) => {
+        if(!err) {
+            res.redirect('employee/list');
+        }
+        else {
+            console.log('Error during record update :' + err);
+        }
+    });
+}
+
+router.get('/list', (req, res) => {
+//   res.json('from list');
+   //docs la tham so thu hai chua cac object database, neu kiem tra ko co loi thi trinh duyet se render ra file list.hbs duoc tao trong folder employee voi thuoc tinh la list va gia tri la docs (noi chua tap hop cac du lieu dang object)
+   Employee.find((err, docs) => {
+       if(!err) {
+           res.render("employee/list", {
+               list: docs
+           });
+       } 
+       else {
+            console.log('Error in retrieving employee list :' + err);
+       }
+   });                           
+
+});
+
+//function handleValiadtionError(err, body) {
+//  for(field in err.errors) {
+//    switch (err.errors[fied].path) {
+//    case "fullName":
+//    body["fullNameError"] = err.errors[field.message];
+//  break;
+//          case "email":
+//          body["emailError"] = err.errors[field.message];
+//       break;
+//    default:
+//  break;
+//        }
+//    }    
+//}
+
+router.get('/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, doc) => {
+        if(!err) {
+            res.render("employee/addOredit", {
+                viewTitle: "UpdateEmployee",
+                employee: doc
+            });
+        }
+    });
+});   
+
+router.get('/delete/:id', (req, res) => {
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        }
+        else {
+            console.log('Error in delete employee :' + err);
+        };
+    })
+})
+
+module.exports= router;
